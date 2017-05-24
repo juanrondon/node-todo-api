3fff506a8e4f3e8852af3bfdc98c186880b3f4eb
Juan Rondon <juanrondonc@outlook.com>
2017-05-24 12:17:40 +1000

    Add GET/todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectId } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -22,13 +23,30 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (error) => {
         res.status(400).send(error);
     });
 });
 
-app.
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send({
+            error: 'Invalid Id'
+        });
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({
+                error: 'Todo not found'
+            });
+        }
+        res.send({ todo });
+    }).catch((error) => {
+        res.status(400).send();
+    });
+});
 
 
 app.listen(3000, () => {
