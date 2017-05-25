e385102abd021e6639b9fbf332e60f0d1d11e1fd
Juan Rondon <juanrondonc@outlook.com>
2017-05-25 15:04:38 +1000

    Add get users me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -47,6 +47,21 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (error) {
+        return Promise.reject();
+    }
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
 UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const _ = require('lodash');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 
@@ -107,6 +108,10 @@ app.post('/users', (req, res) => {
     }).catch((error) => {
         res.status(400).send(error);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 
